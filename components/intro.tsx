372d7ff223743f743bdaddbe79a34cbaad9d4a8e
Jerome Corpuz <jerome.corpuz@edu.sait.ca>
2023-09-19 16:10:45 -0600

    styling animation and hero section text to flex but figure out how to display sm:flex-col

--- components/intro.tsx
+++ components/intro.tsx
@@ -6,29 +6,43 @@ import React from 'react';
 import { motion } from 'framer-motion';
 import { FaGithubAlt } from 'react-icons/fa';
 import { AiOutlineLinkedin } from 'react-icons/ai';
+import animationData from '@/app/animationData.json' 
 
 
 export default function Intro() {
   return (
-    <section className='pt-28'>
-            <div className='w-9/10'>
-               <div className='w-3/4 lg:flex items-center justify-center'>
-                  <div className='bg-blue-50 pl-2 pb-2 w-1/2  border-white border-opacity-40 rounded-md bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem]  dark:bg-gray-950 dark:border-black/40 dark:bg-opacity-75'>
-                        <h1 className='text-custom'>Jerome Corpuz </h1>
-                        <h2>Software developer</h2>
-                        <p>
-                            Mediator for seamless Design-Dev synergy: bridging Creativy and code
-                        </p>
-                        <button>Collaborate</button>
-                        <div className='flex'>
-                            <AiOutlineLinkedin  />
-                            <FaGithubAlt  />
-                        </div>
-
-                  </div>
-        
-               </div>
+    <section className=' flex items-center '>
+            <div className='  p-10 w-1/2  rotate-90 '>
+                <Lottie animationData={animationData}/> 
             </div>
+
+            <motion.div
+              initial={{ opacity: 0}}
+              animate={{ opacity: 1}}
+              transition={{ duration: 0.9 }}
+              className='sm:w-1/2'>
+
+
+                <div className='p-0 w-3/4 lg:flex items-center justify-center'>
+                    <div className=' lg:pl-2 pb-2 '>
+                          <h1 className='text-name'>Jerome Corpuz </h1>
+                          <h2 className=' text-occupation'>Software developer</h2>
+                          <p className=' w-1/2 text-slogan text-yellow-700'>
+                              Mediator for seamless Design-Dev synergy: bridging Creativy and code
+                          </p>
+                          <button className='mt-5 mb-3 pl-5 pr-5 pt-2 pb-2 border-white border-opacity-40 bg-yellow-400 bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:rounded-full dark:bg-gray-950 dark:border-black/40 dark:bg-opacity-75'>Collaborate</button>
+
+                          <div className='flex items-center'>
+                              <AiOutlineLinkedin  />
+                              <FaGithubAlt/>
+                          </div>
+
+                    </div>
+          
+                </div>
+                
+            </motion.div>
+           
 
     </section>
   )
