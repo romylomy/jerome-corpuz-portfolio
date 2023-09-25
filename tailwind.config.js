1cb515e2114fb857e3396e7e54e78d0940a08e75
Jerome Corpuz <jerome.corpuz@edu.sait.ca>
2023-09-24 18:34:44 -0600

    fixing darkmode/light mode issues and overriding default colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,8 +10,8 @@ module.exports = {
     
     extend: {
       colors:{
-        'body-green': '#74be7c',
-        'body-orange':'#ecd5ba',
+        'bodyGreen': '#74be7c',
+        'bodyOrange':'#ecd5ba',
       },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
