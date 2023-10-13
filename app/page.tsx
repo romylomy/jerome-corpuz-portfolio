e5c44782d71adfe0ade22a40204059a3649d8055
Jerome Corpuz <jerome.corpuz@edu.sait.ca>
2023-10-13 02:50:12 -0600

    changed main padding to 5

--- app/page.tsx
+++ app/page.tsx
@@ -7,12 +7,14 @@ import SectionDivider from "@/components/sectionDivider";
 
 export default function Home() {
   return (
-    <main className="shadow-lg p-2 bg-blue-500 bg-opacity-30 backdrop-blur-sm font-Montserrat sm:px-30 md:px-20 lg:px-50 xl:px-60">
+    <main className=" p-5 bg-blue-500 bg-opacity-30 backdrop-blur-sm font-Montserrat sm:px-30 md:px-20 lg:px-50 xl:px-60">
       
       <Intro/>
       <SectionDivider/>
       <Skills />
+
       <About/>
+
       <Projects/>
     </main>
   )
