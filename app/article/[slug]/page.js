06ae8ccd0cb730cc63d56e32cb316bcbf5adf621
Jerome Corpuz <jerome.corpuz@edu.sait.ca>
2024-05-07 20:00:27 -0600

    added padding top 10

--- app/article/[slug]/page.js
+++ app/article/[slug]/page.js
@@ -27,7 +27,7 @@ export default async function Page({ params }) {
   }
 
   return (
-    <div>
+    <div className='pt-20'>
       <Head>
         <title>{page.properties.Title?.title[0].plain_text}</title>
         <link rel="icon" href="/favicon.ico" />
